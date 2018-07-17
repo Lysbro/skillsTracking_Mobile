29ba92c4786f0b794447357ca1e24d357c6f6de3
Lysbro <eleve5@cnampf.com>
2018-07-16 19:08:36 -1000

    logout method created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
+import { HomePage } from './../pages/home/home';
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, App } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
 import { LoginPage } from '../pages/login/login';
+import { ListPage } from '../pages/list/list';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
 
 @Component({
   templateUrl: 'app.html'
@@ -18,14 +18,14 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public authService: AuthServiceProvider, public app: App) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage },
-      { title: 'Login', component: LoginPage }
+      { title: 'Logout', component: HomePage }
     ];
 
   }
@@ -42,6 +42,26 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    // this.nav.setRoot(page.component);
+    if (page.component) {
+      this.nav.setRoot(page.component);
+    } else {
+      this.logout();
+    }
+  }
+
+  logout() {
+    // this.authService.logout().then((result) => {
+    //   this.loading.dismiss();
+    //   let nav = this.app.getRootNav();
+    //   nav.setRoot(LoginPage);
+    // }, (err) => {
+    //   this.loading.dismiss();
+    //   this.presentToast(err);
+    // });
+
+    this.authService.logout();
+    let nav = this.app.getRootNav();
+    nav.setRoot(LoginPage);
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, ToastController, App } from 'ionic-angular';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import { ApiServiceProvider } from '../../providers/api-service/api-service';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,48 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  loading: any;
 
+  constructor(public navParams: NavParams, public navCtrl: NavController, public authService: AuthServiceProvider, public app: App, public apiService: ApiServiceProvider, public loadingCtrl: LoadingController, private toastCtrl: ToastController) {
+    this.apiService.get('formations').then((data) => {
+      console.log(data);
+    });
+  }
+
+  logout() {
+    // this.authService.logout().then((result) => {
+    //   this.loading.dismiss();
+    //   let nav = this.app.getRootNav();
+    //   nav.setRoot(LoginPage);
+    // }, (err) => {
+    //   this.loading.dismiss();
+    //   this.presentToast(err);
+    // });
+
+    this.authService.logout();
+    let nav = this.app.getRootNav();
+    nav.setRoot(LoginPage);
+  }
+
+  showLoader() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Authenticating...'
+    });
+    this.loading.present();
+  }
+
+  presentToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: 'bottom',
+      dismissOnPageChange: true
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+    toast.present();
   }
 
 }
